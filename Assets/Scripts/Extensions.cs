9c283c217053e5c49a7e5e1dc171e4a3323703ee
StrawCraft <strawcraft140904@gmail.com>
2024-11-09 13:43:58 +0700

    .

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -15,4 +15,11 @@ public static class Extensions
         RaycastHit2D hit = Physics2D.CircleCast(rb2D.position, radius, direction, distance, layerMask);
         return hit.collider != null && hit.rigidbody != rb2D;
     }
+
+    public static bool DotTest(this Transform transform, Transform other, Vector2 testDirection)
+    {
+        Vector2 direction = other.position - transform.position;
+        return Vector2.Dot(direction.normalized, testDirection) > 0.25f;
+    }
+
 }
